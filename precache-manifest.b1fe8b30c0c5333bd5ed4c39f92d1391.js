871236f325fe95639afc1bd43ec338dd810ff2a5
nanaaquasi <nanaaquasi@users.noreply.github.com>
2020-05-31 01:04:48 +0000

    Deploy

---
+++ precache-manifest.b1fe8b30c0c5333bd5ed4c39f92d1391.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f52dbefcf63c9f677ace09fcf8bc7d8",
+    "url": "_redirects"
+  },
+  {
+    "revision": "5540f29e7db25f9d1320",
+    "url": "css/app.ab66c29a.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "6f69d99b9b0706a2a955ed42d64742a1",
+    "url": "fonts/roboto-v20-greek-ext_cyrillic-ext_greek_latin-ext_latin_cyrillic_vietnamese-500.6f69d99b.woff2"
+  },
+  {
+    "revision": "851a2b5a8394eb1b868678bfd31a1a8a",
+    "url": "fonts/roboto-v20-greek-ext_cyrillic-ext_greek_latin-ext_latin_cyrillic_vietnamese-500.851a2b5a.woff"
+  },
+  {
+    "revision": "9549360090baf2eb8b25d3a9708fc19d",
+    "url": "fonts/roboto-v20-greek-ext_cyrillic-ext_greek_latin-ext_latin_cyrillic_vietnamese-regular.95493600.woff2"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "fonts/roboto-v20-greek-ext_cyrillic-ext_greek_latin-ext_latin_cyrillic_vietnamese-regular.a91ad097.woff"
+  },
+  {
+    "revision": "bc995e14383a6693d6a5518728e11fff",
+    "url": "img/bott.bc995e14.png"
+  },
+  {
+    "revision": "b0cc35676d83990c18a7d7f627fc2455",
+    "url": "img/bubble-left.b0cc3567.svg"
+  },
+  {
+    "revision": "18d299d40139a3b474261c1398598129",
+    "url": "img/bubble-left_dark.18d299d4.svg"
+  },
+  {
+    "revision": "f2e94057e60f1b5c2def2039b1819c45",
+    "url": "img/bubble-right.f2e94057.svg"
+  },
+  {
+    "revision": "d2bb39b2ba15165319ad8374060e4444",
+    "url": "img/bubble-right_dark.d2bb39b2.svg"
+  },
+  {
+    "revision": "2b9cb912b95c35649da76ac7e3f9ce25",
+    "url": "img/opengraph.png"
+  },
+  {
+    "revision": "f5b3aa18d972f4189bb54c0b67f3fb15",
+    "url": "index.html"
+  },
+  {
+    "revision": "5540f29e7db25f9d1320",
+    "url": "js/app.0bf846f8.js"
+  },
+  {
+    "revision": "a7050db801ccfe90eb4b",
+    "url": "js/chunk-vendors.b588d2ed.js"
+  },
+  {
+    "revision": "bfae9faebbd4ca00d77737a88e057227",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "50d8a018e8ae96732c8a2ba663c61d4e",
+    "url": "robots.txt"
+  }
+]);
